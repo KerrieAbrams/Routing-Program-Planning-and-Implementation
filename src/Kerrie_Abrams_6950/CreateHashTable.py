4f9990c5376829a4e7f61e29f91e561c8bca78a2
kabra70 <kabra70@wgu.edu>
2024-04-11 13:07:41 -0400

    created hash table function & remove and print functions for table

---
+++ src/Kerrie_Abrams_6950/CreateHashTable.py
@@ -0,0 +1,58 @@
+class HashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    def __init__(self, initial_capacity=40):
+        # initialize the hash table with empty bucket list entries.
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    #Get bucket list where the item belongs
+    def _get_bucket_list(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        return bucket_list
+
+    # Inserts a new item into the hash table.
+    def insert(self, key, item): #  does both insert and update
+        # call _get_bucket_list to retrieve corresponding bucket list
+        bucket_list = self._get_bucket_list(key)
+
+        # update key if it is already in the bucket
+        for kv in bucket_list:
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        # if not, insert the item to the end of the bucket list.
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
+    def search(self, key):
+        # call _get_bucket_list to retrieve corresponding bucket list
+        bucket_list = self._get_bucket_list(key)
+
+        # search for the key in the bucket list
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1] # value
+        return None
+
+    # Removes an item with matching key from the hash table.
+    def remove(self, key):
+        # call _get_bucket_list to retrieve corresponding bucket list
+        bucket_list = self._get_bucket_list(key)
+
+        # remove the item from the bucket list if it is present.
+        for kv in bucket_list:
+            #print (key_value)
+            if kv[0] == key:
+                bucket_list.remove([kv[0],kv[1]])
+
+    def print(self):
+        for item in self.table:
+            if item is not None:
+                print(str(item))
--- src/Kerrie_Abrams_6950/Package.py
+++ src/Kerrie_Abrams_6950/Package.py
@@ -1,5 +1,5 @@
 class Package:
-    def __init__(self, package_id, address, city, state, zipcode, deadline_time, weight, departure_time, arrival_time):
+    def __init__(self, package_id, address, city, state, zipcode, deadline_time, weight, departure_time, arrival_time, status):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -9,4 +9,7 @@ class Package:
         self.weight = weight
         self.departure_time = departure_time
         self.arrival_time = arrival_time
+        self.status = status
 
+    def __str__(self):
+        return self.package_id
