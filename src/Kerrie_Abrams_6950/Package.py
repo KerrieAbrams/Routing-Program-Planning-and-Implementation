23573058284b64d939364c98f7f4b5217fb69954
kabra70 <kabra70@wgu.edu>
2024-04-11 11:49:28 -0400

    created main files and added __init__() to Package.py

---
+++ src/Kerrie_Abrams_6950/Package.py
@@ -0,0 +1,12 @@
+class Package:
+    def __init__(self, package_id, address, city, state, zipcode, deadline_time, weight, departure_time, arrival_time):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self. zipcode = zipcode
+        self.deadline_time = deadline_time
+        self.weight = weight
+        self.departure_time = departure_time
+        self.arrival_time = arrival_time
+
