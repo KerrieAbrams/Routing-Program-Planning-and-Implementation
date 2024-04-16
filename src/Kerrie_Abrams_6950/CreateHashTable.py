72c238566528cd2b4f466760d97da573bca04a4c
kabra70 <kabra70@wgu.edu>
2024-04-16 19:32:28 -0400

    updated class files

--- src/Kerrie_Abrams_6950/CreateHashTable.py
+++ src/Kerrie_Abrams_6950/CreateHashTable.py
@@ -1,7 +1,7 @@
 class HashTable:
     # Constructor with optional initial capacity parameter.
     # Assigns all buckets with an empty list.
-    def __init__(self, initial_capacity=40):
+    def __init__(self, initial_capacity=10):
         # initialize the hash table with empty bucket list entries.
         self.table = []
         for i in range(initial_capacity):
@@ -15,7 +15,6 @@ class HashTable:
 
     # Inserts a new item into the hash table.
     def insert(self, key, item):
-        # call _get_bucket_list to retrieve corresponding bucket list
         bucket_list = self._get_bucket_list(key)
 
         # update key if it is already in the bucket
@@ -23,7 +22,6 @@ class HashTable:
             if kv[0] == key:
                 kv[1] = item
                 return True
-
         # if not, insert the item to the end of the bucket list.
         key_value = [key, item]
         bucket_list.append(key_value)
@@ -32,9 +30,7 @@ class HashTable:
     # Searches for an item with matching key in the hash table.
     # Returns the item if found, or None if not found.
     def search(self, key):
-        # call _get_bucket_list to retrieve corresponding bucket list
         bucket_list = self._get_bucket_list(key)
-
         # search for the key in the bucket list
         for kv in bucket_list:
             if kv[0] == key:
@@ -43,7 +39,6 @@ class HashTable:
 
     # Removes an item with matching key from the hash table.
     def remove(self, key):
-        # call _get_bucket_list to retrieve corresponding bucket list
         bucket_list = self._get_bucket_list(key)
 
         # remove the item from the bucket list if it is present.
